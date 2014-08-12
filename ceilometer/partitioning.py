36aa8c3a815c92238d1787cde1726344c129d572
Nejc Saje <nsaje@redhat.com>
2014-08-12 08:12:08 +0200

    before switch to explicit
    Change-Id: I79f49a42edc62f41b69fb069ac99f272262b91bc

--- ceilometer/partitioning.py
+++ ceilometer/partitioning.py
@@ -7,7 +7,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -15,7 +15,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import atexit
 import functools
 import hashlib
 import collections
@@ -40,74 +39,84 @@ OPTS = [
 ]
 cfg.CONF.register_opts(OPTS)
 
-_coordinator = None
-_our_id = uuid.uuid4()
-_group_members = collections.defaultdict(set)
-
-if cfg.CONF.backend_url:
-    _coordinator = tooz.coordination.get_coordinator(
-        cfg.CONF.coordination_backend_url, _our_id)
-    def heartbeat():
-        while True:
-            _coordinator.heartbeat()
-            _coordinator.run_watchers()
-            eventlet.sleep(cfg.CONF.coordination_heartbeat)
-    eventlet.spawn_n(heartbeat)
-
-    def stop():
-        _coordinator.stop()
-    atexit.register(stop)
+_group_manager = None
 
 
-def _group_join_handler(event):
-    _group_members[event.group_id] += event.member_id
+def get_group_manager(conf):
+    global _group_manager
+    if not _group_manager:
+        _group_manager = _GroupManager(conf)
 
 
-def _group_leave_handler(event):
-    _group_members[event.group_id] -= event.member_id
+class _GroupManager:
+    """Group manager, one instance per agent"""
+    coordinator = None
+    our_id = uuid.uuid4()
+    group_members = collections.defaultdict(set)
 
+    def __init__(self, conf):
+        if conf.coordination_backend_url:
+            self.coordinator = tooz.coordination.getcoordinator(
+                conf.coordination_backend_url, self.our_id)
 
-def _join_group(group_id):
-    joined = False
-    while not joined:
-        join_req = _coordinator.join_group()
-        try:
-            join_req.get()
-        except tooz.MemberAlreadyExist:
-            return
-        except tooz.GroupNotCreated:
-            create_grp_req = _coordinator.create_group(group_id)
+            def heartbeat():
+                while True:
+                    self.coordinator.heartbeat()
+                    self.coordinator.run_watchers()
+                    eventlet.sleep(conf.coordination_heartbeat)
+
+            eventlet.spawn_n(heartbeat)
+
+    def group_join_handler(self, event):
+        self.group_members[event.group_id] += event.member_id
+
+    def group_leave_handler(self, event):
+        self.group_members[event.group_id] -= event.member_id
+
+    def join_group(self, group_id):
+        joined = False
+        while not joined:
+            join_req = self.coordinator.join_group()
             try:
-                create_grp_req.get()
-            except tooz.GroupAlreadyExist:
-                pass
-            continue
-        joined = True
+                join_req.get()
+            except tooz.MemberAlreadyExist:
+                return
+            except tooz.GroupNotCreated:
+                create_grp_req = self.coordinator.create_group(group_id)
+                try:
+                    create_grp_req.get()
+                except tooz.GroupAlreadyExist:
+                    pass
+                continue
+            joined = True
 
-    get_members_req = _coordinator.get_members(group_id)
-    _group_members[group_id] += get_members_req.get()
+        get_members_req = self.coordinator.get_members(group_id)
+        self.group_members[group_id] += get_members_req.get()
 
-    _coordinator.watch_join_group(group_id, _group_join_handler)
-    _coordinator.watch_leave_group(group_id, _group_leave_handler)
+        self.coordinator.watch_join_group(group_id, self.group_join_handler)
+        self.coordinator.watch_leave_group(group_id, self.group_leave_handler)
 
-
-def _get_members(group_id):
-    return _group_members[group_id]
+    def get_members(self, group_id):
+        return self.group_members[group_id]
 
 
 class Partition:
     """ Work partitioning decorator
     """
+
     def __init__(self, func, group_id="central_agent_global"):
         self.func = func
         functools.update_wrapper(self, func)
-        _join_group(group_id)
+        if not _group_manager:
+            #_group_manager = _GroupManager(cfg.CONF)
+            raise Exception('no group manager!') # TODO prettier exception
+        _group_manager.join_group(group_id)
 
     def __call__(self, *args, **kwargs):
         obj = self.func(*args, **kwargs)
         if hasattr(obj, '__iter__'):
             members = _get_members()
-            our_key = sorted(members).index(_our_id)
+            our_key = sorted(members).index(self.our_id)
             return [v for v in obj
                     if int(hashlib.md5(str(v)).hexdigest(), 16)
                     % len(members) == our_key]
